27db274ae0333aa03a8308ab2618530399af0843
Mustak <Mustak@DESKTOP-OA4LL8B>
2020-11-27 15:54:30 +0600

    Add new Api

--- BSPTestWebApp/Controllers/PostApiController.cs
+++ BSPTestWebApp/Controllers/PostApiController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Web.Http;
 using System.Data;
 using System.Data.Entity;
+using PTestWebApi.Models;
 
 namespace BSPTestWebApp.Controllers
 {
@@ -13,15 +14,31 @@ namespace BSPTestWebApp.Controllers
         public IHttpActionResult GetPosts()
         {
             var result = db.Posts.Include(p => p.Comments)
-                .Select(p => new {
+                .Select(p => new
+                {
                     Content = p.Content,
                     Creator = p.Creator,
                     CreateDate = p.Creator
                 ,
-                    Comment = p.Comments.Select(r => new { id = r.Id, CommentText = r.CommentText, Creator = r.Creator,CreateDate=r.CreateDate, Like = r.LikeCounter, Dislike = r.DislikeCounter })
+                    Comment = p.Comments.Select(r => new { id = r.Id, CommentText = r.CommentText, Creator = r.Creator, CreateDate = r.CreateDate, Like = r.LikeCounter, Dislike = r.DislikeCounter })
                 }).ToList();
 
             return Ok(new { results = result });
+
+        }
+
+        [Route("GetPosts")]
+        public IHttpActionResult CountLikeDislike(int isLike, int commentId)
+        {
+            Comment comment = new Comment();
+            comment = db.Comments.Find(commentId);
+            if (isLike == 1)
+                comment.LikeCounter++;
+            else
+                comment.DislikeCounter++;
+            db.Entry(comment).State = EntityState.Modified;
+            db.SaveChanges();
+            return Ok();
 
         }
     }
